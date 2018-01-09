17b15deca97bb152f3a62405a0adc778ae904806
Carlos Eduardo Alves de Godoi <carloseduardoalvesgodoi@gmail.com>
2018-01-09 00:02:01 -0200

    Arquivo com as Definições do Banco de Dados

---
+++ db_ddl/ddl.sql
@@ -0,0 +1,13 @@
+create database del;
+
+use del;
+
+create table usuario
+(
+id smallint(4) unsigned not null primary key auto_increment,
+login varchar(10) unique not null,
+senha varchar(20) not null,
+nome varchar(45) not null,
+index idx_login(login),
+index idx_nome(nome)
+);
